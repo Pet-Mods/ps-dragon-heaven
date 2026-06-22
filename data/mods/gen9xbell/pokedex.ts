4500c282012e8d29b4604580bccf2a148d53a45e
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-06-22 17:19:20 -0400

    xbellpaldea commit 1

--- data/mods/gen9xbell/pokedex.ts
+++ data/mods/gen9xbell/pokedex.ts
@@ -1,20 +1,177 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
 
-/*	example : {
- *		num: -1,														// Can be positive or negative, but don't use multiple IDs if they are not the same baseSpecies
- *		name: "",
- *		types: ["", ""],
- *		genderRatio: { M: 0.875, F: 0.125 }, 							// Optional
- *		baseStats: { hp: 1, atk: 1, def: 1, spa: 1, spd: 1, spe: 1 },
- *		abilities: { 0: "", 1: "", H: "" }, 							// Only 0 needs to be included (can be No Ability)
- *		heightm: 0, 													// Optional
- *		weightkg: 0, 													// Only required if your mod uses weight based moves (Grass Knot, Low Kick, Heat Crash, Heavy Slam)
- *		color: "",														// Optional
- *		evos: [""], 													// Optional, but required for Eviolite to work
- *		eggGroups: ["", ""], 											// Optional
- *		otherFormes: [""],												// Use if the species has more than one forme.
-		formeOrder: ["", ""],											// Base Species, then formes in alphabetical order
- *	},
- */
-
+	aggron: {
+		inherit: true,
+	},
+	alcremie: {
+		inherit: true,
+	},
+	altaria: {
+		inherit: true,
+	},
+	armarouge: {
+		inherit: true,
+	},
+	articuno: {
+		inherit: true,
+	},
+	barraskewda: {
+		inherit: true,
+	},
+	bastiodon: {
+		inherit: true,
+	},
+	bibarel: {
+		inherit: true,
+	},
+	breloom: {
+		inherit: true,
+	},
+	calyrexice: {
+		inherit: true,
+	},
+	carbink: {
+		inherit: true,
+	},
+	ceruledge: {
+		inherit: true,
+	},
+	comfey: {
+		inherit: true,
+	},
+	cresselia: {
+		inherit: true,
+	},
+	duraludon: {
+		inherit: true,
+	},
+	eelektross: {
+		inherit: true,
+	},
+	electivire: {
+		inherit: true,
+	},
+	florges: {
+		inherit: true,
+	},
+	fluttermane: {
+		inherit: true,
+	},
+	forretress: {
+		inherit: true,
+	},
+	furfrou: {
+		inherit: true,
+	},
+	golisopod: {
+		inherit: true,
+	},
+	gyarados: {
+		inherit: true,
+	},
+	haxorus: {
+		inherit: true,
+	},
+	heatran: {
+		inherit: true,
+	},
+	ironleaves: {
+		inherit: true,
+	},
+	jumpluff: {
+		inherit: true,
+	},
+	klinklang: {
+		inherit: true,
+	},
+	lucario: {
+		inherit: true,
+	},
+	luxray: {
+		inherit: true,
+	},
+	mamoswine: {
+		inherit: true,
+	},
+	mewtwo: {
+		inherit: true,
+	},
+	ogerponhearthflame: {
+		inherit: true,
+	},
+	overqwil: {
+		inherit: true,
+	},
+	palossand: {
+		inherit: true,
+	},
+	parasect: {
+		inherit: true,
+	},
+	passimian: {
+		inherit: true,
+	},
+	pincurchin: {
+		inherit: true,
+	},
+	pinsir: {
+		inherit: true,
+	},
+	polteageist: {
+		inherit: true,
+	},
+	regigigas: {
+		inherit: true,
+	},
+	registeel: {
+		inherit: true,
+	},
+	rotomfan: {
+		inherit: true,
+	},
+	rotomwash: {
+		inherit: true,
+	},
+	salazzle: {
+		inherit: true,
+	},
+	simipour: {
+		inherit: true,
+	},
+	sinistcha: {
+		inherit: true,
+	},
+	sirfetchd: {
+		inherit: true,
+	},
+	staraptor: {
+		inherit: true,
+	},
+	steelix: {
+		inherit: true,
+	},
+	steelixmega: {
+		inherit: true,
+	},
+	tapubulu: {
+		inherit: true,
+	},
+	terrakion: {
+		inherit: true,
+	},
+	torterra: {
+		inherit: true,
+	},
+	turtonator: {
+		inherit: true,
+	},
+	vikavolt: {
+		inherit: true,
+	},
+	wochien: {
+		inherit: true,
+	},
+	wormadamtrash: {
+		inherit: true,
+	},
 };
